8a8586f57c04ba44b19479e07595906dca43f345
Stobio19 <stobio@utb.edu.co>
2023-10-08 19:14:33 -0500

    Implementación SSO

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { LoginButton } from "./Login";
+import { SignUpbutton } from "./signup";
+import { LogoutButton } from "./Logout";
+import { Profile } from "./Profile";
+import "./App.css";
+import "./index.css";
+
+function App() {
+  // Definining the state of the user
+  const { isAuthenticated } = useAuth0();
+
+  // <header className="App-header"></header>
+  return (
+    <div className="App">
+      {isAuthenticated ? (
+        <>
+          <div className="container-logout">
+            <Profile className="profile" />
+            <LogoutButton className="button" />
+          </div>
+        </>
+      ) : (
+        // Div className body which contains all of the content of the first (main) page
+        <div className="body">
+          <h1 className="main-title">Welcome to our page!</h1>
+          <div className="container-login">
+            <p className="title">Try this!</p>
+            <LoginButton />
+            <SignUpbutton />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/signup.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./index.css"
+import { useAuth0 } from "@auth0/auth0-react"
+
+export const SignUpbutton = () => {
+    const { loginWithRedirect } = useAuth0(); 
+    return < button className="button" onClick={() => loginWithRedirect({ authorizationParams:{ screen_hint: 'signup'}})}>Sign Up</button>
+};
+
+export default SignUpbutton;
