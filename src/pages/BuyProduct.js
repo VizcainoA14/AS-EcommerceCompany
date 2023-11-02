1b617874ea8219fcc54282636a22743eca0f5740
VizcainoA14 <128247244+VizcainoA14@users.noreply.github.com>
2023-11-01 20:56:38 -0500

    Implementacion Servicio ver producto

--- src/pages/BuyProduct.js
+++ src/pages/BuyProduct.js
@@ -10,59 +10,51 @@ import { useParams } from "react-router-dom";
 
 export const Buy = () => {
   const { id } = useParams();
-  const get_token = useAuth0().getIdTokenClaims();
   const [error, setError] = useState(null);
-  const { getIdTokenClaims } = useAuth0();
   const [accessToken, setAccessToken] = useState("");
+  const [products, setProducts] = useState([]);
 
 
-  const showproduct = async (id) => {
+  const get_product = async (id) => {
     try {
-      let accessToken = await get_token;
-      get_token.then((result) => {
-        setAccessToken(result.__raw);
-      });
-      const token = accessToken.__raw;
-      await fetch(`https://backend-ecommerce-api-fcrd.onrender.com/GET_PRODUCT/${id}`, {
+      const response = await fetch(`https://backend-ecommerce-api-fcrd.onrender.com/GET_PRODUCT/?id_product=${id}`, {
         method: "GET",
         headers: {
-          Authorization: "Bearer " + token,
           "Content-Type": "application/json",
         },
       });
+      if (!response.ok) {
+        throw new Error("Network response was not OK");
+      }
+      const json = await response.json();
+      setProducts(json);
     } catch (error) {
-      console.error(error);
-    }
-  };
+      setError(error);
+    } 
   
-  // Resto de tu código...
+  };
+
   
   useEffect(() => {
-    console.log(id);
-    showproduct(id);
+    get_product(id);
   }, []);
   
 
 
-
-
-
-
-
-  
   return (
     <div className="main-content-buy-product">
       <section className="section-1-buy-product">
+      {products.map((products) => (
         <div className="section-buy-product-div">
-          <div className="section-buy-product-div-specification-content-1">
+        <div className="section-buy-product-div-specification-content-1">
             <h2>AQUI VA LA IMAGEN</h2>
             <div className="buy-product-main-img-container">
-            <img src={shop_02} alt="main-img-example"/>
+            <img src={products.image_1} alt="main-img-example"/>
             </div>
           </div>
           <div className="section-buy-product-div-specification-content-2">
-            <h2 className="buy-product-name">NOMBRE DEL PRODUCTO</h2>
-            <h3 className="buy-product-price">$25.000</h3>
+            <h2 className="buy-product-name">{products.name_product}</h2>
+            <h3 className="buy-product-price">${products.price}</h3>
             <div className="buy-product-stars">
               <div className="star">
                 <FontAwesomeIcon icon={faStar} />
@@ -80,21 +72,20 @@ export const Buy = () => {
                 <FontAwesomeIcon icon={faStar} />
               </div>
             </div>
-            <span className="bold-text span-buy-product-content-2">Description:</span> <p className="paragraph-buy-product-content-2">Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit, sed do eiusmod temp
-            incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.
-            Donec condimentum elementum convallis. Nunc sed orci a diam ultrices
-            aliquet interdum quis nulla.</p>
-            <span className="bold-text span-buy-product-content-2 ">Available (Stock): 4{/* Here comes stock*/}</span>
+            <span className="bold-text span-buy-product-content-2">Description:</span> <p className="paragraph-buy-product-content-2">
+              {products.description}</p>
+            <span className="bold-text span-buy-product-content-2 ">Available (Stock): {products.stock}</span>
             <form className="form-buy-product">
               <button className="buy-product-submit-button" type="submit">Buy</button>
               <button className="buy-product-submit-button" type="submit">Add To Cart</button>
             </form>
           </div>
         </div>
+      ))}
       </section>
       <section className="section-2-buy-product"></section>
     </div>
+    
   );
 };
 
